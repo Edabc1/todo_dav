5287bbd9851c1a57143580016f933992d9563341
Edabc1_Gamer <edabc1.umeda@gmail.com>
2024-09-03 02:37:56 +0000

    2.0 - ícones branco

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Aplicativo de Notas",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
